ca5252eda45bd0fec9157e2b7354dc81bd947dc1
NotGuatas <josephaugustoflores@gmail.com>
2024-10-07 22:28:27 -0500

    Agregar archivos de proyecto.

---
+++ Data/JosephFloresCodeFirstContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JosephFloresCodeFirst.Models;
+
+namespace JosephFloresCodeFirst.Data
+{
+    public class JosephFloresCodeFirstContext : DbContext
+    {
+        public JosephFloresCodeFirstContext (DbContextOptions<JosephFloresCodeFirstContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JosephFloresCodeFirst.Models.Burger> Burger { get; set; } = default!;
+    }
+}
